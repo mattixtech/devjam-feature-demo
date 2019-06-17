5dc8962028ba4b8db9b3c8d3f23f85dc54b62e7d
Matthew Brooks <matt@opennms.com>
2019-06-17 11:51:00 -0400

    Demo

---
+++ command/src/main/java/org/opennms/features/counter/command/CounterCommand.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2019 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.counter.command;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.opennms.features.counter.api.CounterService;
+
+// Executed in Karaf shell via opennms-demo:count
+@Command(scope = "opennms-demo", name = "count", description = "Get the count")
+@Service
+public class CounterCommand implements Action {
+    @Reference
+    private CounterService counterService;
+
+    @Override
+    public Object execute() {
+        System.out.println("The counterService's configured name is: " + counterService.getName());
+        System.out.println("The count for alarms is: " + counterService.getNumAlarms());
+        System.out.println("The count for things is: " + counterService.getNumThings());
+        return null;
+    }
+}
---
+++ counter/src/main/java/org/opennms/features/counter/CounterServiceImpl.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2019 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.counter;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.opennms.features.counter.api.CounterService;
+import org.opennms.features.counter.api.ThingHandler;
+import org.opennms.features.counter.api.ThingSupplierService;
+import org.opennms.integration.api.v1.alarms.AlarmLifecycleListener;
+import org.opennms.integration.api.v1.dao.AlarmDao;
+import org.opennms.integration.api.v1.model.Alarm;
+
+public class CounterServiceImpl implements AlarmLifecycleListener, CounterService {
+    private final AtomicLong numAlarms = new AtomicLong(0);
+    private final AtomicLong numThings = new AtomicLong(0);
+    private final String name;
+
+    // Obtained via integration API
+    private final AlarmDao alarmDao;
+    
+    // How to react to our thing handler
+    private final ThingHandler thingHandler = numThings::getAndIncrement;
+
+    public CounterServiceImpl(AlarmDao alarmDao, String name) {
+        this.alarmDao = Objects.requireNonNull(alarmDao);
+        this.name = Objects.requireNonNull(name);
+        initialize();
+    }
+
+    private void initialize() {
+        numAlarms.set(alarmDao.getAlarmCount());
+    }
+
+    public void bindThingSupplier(ThingSupplierService thingSupplierService) {
+        thingSupplierService.registerThingHandler(thingHandler);
+        numThings.getAndAdd(thingSupplierService.getNumThings());
+    }
+
+    public void unbindThingSupplier(ThingSupplierService thingSupplierService) {
+        // no-op
+    }
+
+    public void handleAlarmSnapshot(List<Alarm> list) {
+        // no-op
+    }
+
+    public void handleNewOrUpdatedAlarm(Alarm alarm) {
+        numAlarms.incrementAndGet();
+    }
+
+    public void handleDeletedAlarm(int i, String s) {
+        numAlarms.decrementAndGet();
+    }
+
+    public long getNumAlarms() {
+        return numAlarms.get();
+    }
+
+    public long getNumThings() {
+        return numThings.get();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
